19e960e84226e1f05764dddc88140c7a8096465b
Will van Noordt <willvanzero@gmail.com>
2022-05-17 18:02:03 +0100

    I made a huge mess

--- src/navier-stokes/convective.h
+++ src/navier-stokes/convective.h
@@ -5,54 +5,46 @@
 
 #include "navier-stokes/fluid_state.h"
 #include "navier-stokes/flux_base.h"
-#include "navier-stokes/flux_input.h"
 
 namespace cvdf::convective
-{
-    template <class T, class state_t>
-    concept lr_conv_flux_function = fluid_state::is_state_type<state_t> && 
-    requires(
-        T t,
-        state_t ql,
-        state_t qr,
-        const ctrs::array<typename state_t::value_type,3>& normal_l,
-        const ctrs::array<typename state_t::value_type,3>& normal_r)
-    {
-        { t.calc_flux(ql, qr, normal_l, normal_r) } -> cvdf::ctrs::basic_array;
-    };
-    
+{    
     template <fluid_state::state_dependent_gas gas_t> struct totani_lr
-    : public flux_base::flux_base_t<
-        totani_lr,
-        fluid_state::flux_t<dtype>,
-        flux_input::flux_input_t<
-            flux_input::left_right<cell_state<fluid_state::prim_t<dtype>>, cell_normal<fluid_state::prim_t<dtype>>>
-            >
+    : public flux_base::flux_base_t
+    <
+        totani_lr<gas_t>,
+        fluid_state::flux_t<typename gas_t::value_type>,
+        flux_base::flux_input_t
+        <
+            flux_base::left_right<>,
+            flux_base::face_info<>
+        >
+    >
     {
         totani_lr(const gas_t& gas_in) {gas = &gas_in;}
         template <typename dtype> fluid_state::flux_t<dtype>
-        calc_flux(
-            const fluid_state::prim_t<dtype>& ql,
-            const fluid_state::prim_t<dtype>& qr,
-            const ctrs::array<dtype,3>& normal_l,
-            const ctrs::array<dtype,3>& normal_r) const override final
+        calc_flux(const auto& input) const
         {
             fluid_state::flux_t<dtype> output;
-            dtype un_l = normal_l[0]*ql.u()+normal_l[1]*ql.v()+normal_l[2]*ql.w();
-            dtype un_r = normal_r[0]*qr.u()+normal_r[1]*qr.v()+normal_r[2]*qr.w();
+            // const auto& ql = input.cell_data.left.get<0>.data;
+            // const auto& qr = input.cell_data.right.get<0>.data;
+            // const auto& normal_l = input.cell_data.left.get<1>.data;
+            // const auto& normal_r = input.cell_data.right.get<1>.data;
             
-            dtype rho_l = ql.p()/(gas->get_R(ql)*ql.T());
-            dtype rho_r = qr.p()/(gas->get_R(qr)*qr.T());
-            
-            dtype e_l = ql.p()/(rho_l*(gas->get_gamma(ql)-1.0));
-            dtype e_r = qr.p()/(rho_r*(gas->get_gamma(qr)-1.0));
-            
-            dtype c = 0.25*(rho_l+rho_r)*(un_l+un_r);
-            output.continuity() = c;
-            output.energy()     = 0.5*c*(e_l + e_r + ql.u()*qr.u() + ql.v()*qr.v() + ql.w()*qr.w()) + 0.5*(un_l*qr.p() + un_r*ql.p());
-            output.x_momentum() = 0.5*c*(ql.u()+qr.u()) + 0.5*(normal_l[0]*ql.p()+normal_r[0]*qr.p());
-            output.y_momentum() = 0.5*c*(ql.v()+qr.v()) + 0.5*(normal_l[1]*ql.p()+normal_r[1]*qr.p());
-            output.z_momentum() = 0.5*c*(ql.w()+qr.w()) + 0.5*(normal_l[2]*ql.p()+normal_r[2]*qr.p());
+            // dtype un_l = normal_l[0]*ql.u()+normal_l[1]*ql.v()+normal_l[2]*ql.w();
+            // dtype un_r = normal_r[0]*qr.u()+normal_r[1]*qr.v()+normal_r[2]*qr.w();
+            // 
+            // dtype rho_l = ql.p()/(gas->get_R(ql)*ql.T());
+            // dtype rho_r = qr.p()/(gas->get_R(qr)*qr.T());
+            // 
+            // dtype e_l = ql.p()/(rho_l*(gas->get_gamma(ql)-1.0));
+            // dtype e_r = qr.p()/(rho_r*(gas->get_gamma(qr)-1.0));
+            // 
+            // dtype c = 0.25*(rho_l+rho_r)*(un_l+un_r);
+            // output.continuity() = c;
+            // output.energy()     = 0.5*c*(e_l + e_r + ql.u()*qr.u() + ql.v()*qr.v() + ql.w()*qr.w()) + 0.5*(un_l*qr.p() + un_r*ql.p());
+            // output.x_momentum() = 0.5*c*(ql.u()+qr.u()) + 0.5*(normal_l[0]*ql.p()+normal_r[0]*qr.p());
+            // output.y_momentum() = 0.5*c*(ql.v()+qr.v()) + 0.5*(normal_l[1]*ql.p()+normal_r[1]*qr.p());
+            // output.z_momentum() = 0.5*c*(ql.w()+qr.w()) + 0.5*(normal_l[2]*ql.p()+normal_r[2]*qr.p());
             return output;
         }
         
--- src/navier-stokes/flux_algs.h
+++ src/navier-stokes/flux_algs.h
@@ -3,13 +3,12 @@
 #include "core/grid.h"
 
 #include "navier-stokes/convective.h"
-#include "navier-stokes/flux_input.h"
 
 namespace cvdf::flux_algs
 {
     template <
         grid::multiblock_array array_t,
-        convective::lr_conv_flux_function<fluid_state::prim_t<typename array_t::value_type>> flux_func_t>
+        typename flux_func_t>
     requires
         grid::has_centering_type<array_t, grid::cell_centered> &&
         (dims::rank_eval<typename array_t::array_minor_dim_t>::value==1) &&
@@ -37,13 +36,13 @@ namespace cvdf::flux_algs
             for (int n = 0; n < ql.size(); ++n) ql[n] = prims(n, il[0], il[1], il[2], il[3]);
             for (int n = 0; n < qr.size(); ++n) qr[n] = prims(n, ir[0], ir[1], ir[2], ir[3]);
             // calc_gradient(prims, ar_grid.coord_sys());
-            fluid_state::flux_t<real_type> flux = flux_func.calc_flux(ql, qr, nvec_l, nvec_r);
+            // fluid_state::flux_t<real_type> flux = flux_func.calc_flux(ql, qr, nvec_l, nvec_r);
             const real_type dx = ar_grid.get_dx(idir);
-            for (int n = 0; n < flux.size(); ++n)
-            {
-                rhs(n, il[0], il[1], il[2], idx[3])-=jac_l*flux[n]/(dx);
-                rhs(n, ir[0], ir[1], ir[2], idx[3])+=jac_r*flux[n]/(dx);
-            }
+            // for (int n = 0; n < flux.size(); ++n)
+            // {
+            //     rhs(n, il[0], il[1], il[2], idx[3])-=jac_l*flux[n]/(dx);
+            //     rhs(n, ir[0], ir[1], ir[2], idx[3])+=jac_r*flux[n]/(dx);
+            // }
         }
     }
 }
