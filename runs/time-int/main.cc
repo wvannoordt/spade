6e918a3e54244dc5ec60684af00446ebb0bf5751
Will van Noordt <willvanzero@gmail.com>
2022-05-19 11:12:29 +0100

    verified time integration

--- runs/time-int/main.cc
+++ runs/time-int/main.cc
@@ -1,12 +1,30 @@
 #include "cvdf.h"
 
 typedef double real_t;
-typedef cvdf::ctrs::array<real_t, 3> v3d;
-typedef cvdf::ctrs::array<int,    3> v3i;
-typedef cvdf::ctrs::array<int,    4> v4i;
 
 int main(int argc, char** argv)
 {
-    cvdf::time_integration::rk2 time_int;
+    real_t rhs1, rhs2, var;
+    real_t t0 = 0.0;
+    real_t t1 = 5.0*cvdf::consts::pi;
+    int nt = 250;
+    real_t dt = (t1 - t0) / (nt);
+    var = -1.0;
+    
+    auto calc_rhs = [&](real_t& rhs, const real_t& q, const real_t& time) -> void
+    {
+        rhs = 0.0;
+        rhs += sin(time);
+    };
+    cvdf::time_integration::identity_transform_t<real_t> trans;
+    cvdf::time_integration::rk2 time_int(var, rhs1, rhs2, t0, dt, calc_rhs, trans, trans);
+    std::ofstream myfile("soln.dat");
+    for (int i = 0; i < nt; ++i)
+    {
+        time_int.advance();
+        print(time_int.time(), time_int.soln(), -cos(time_int.time()));
+        myfile << time_int.time() << " " << time_int.soln() << " " << -cos(time_int.time()) << std::endl;
+    }
+    
     return 0;
 }
--- src/core/time_integration.h
+++ src/core/time_integration.h
@@ -2,8 +2,66 @@
 
 namespace cvdf::time_integration
 {
+    template <typename state_trans_t> struct identity_transform_t
+    {
+        void operator() (state_trans_t& state) const {}
+    };
+    template <
+        typename var_state_t,
+        typename rhs_state_t,
+        typename time_state_t,
+        typename rhs_calc_t,
+        typename state_trans_t,
+        typename inv_state_trans_t
+        >
     struct rk2
     {
+        time_state_t t;
+        time_state_t dt;
+        var_state_t* q;
+        rhs_state_t* k0;
+        rhs_state_t* k1;
+        const rhs_calc_t*  rhs_oper;
+        const state_trans_t* state_trans;
+        const inv_state_trans_t* inv_state_trans;
+        rk2(
+            var_state_t& q_in,
+            rhs_state_t& k0_in,
+            rhs_state_t& k1_in,
+            const time_state_t& t0_in,
+            const time_state_t& dt_in,
+            const rhs_calc_t& rhs_oper_in,
+            const state_trans_t& trans_in,
+            const inv_state_trans_t& inv_trans_in)
+        {
+            dt = dt_in;
+            q  = &q_in;
+            t  = t0_in;
+            k0 = &k0_in;
+            k1 = &k1_in;
+            rhs_oper = &rhs_oper_in;
+            state_trans = &trans_in;
+            inv_state_trans = &inv_trans_in;
+        }
         
+        void advance()
+        {
+            (*rhs_oper)((*k0), (*q), t);
+            (*k0) *= (0.5*dt);
+            (*state_trans)((*q));
+            (*q) += (*k0);
+            (*inv_state_trans)((*q));
+            t += 0.5*dt;
+            (*rhs_oper)((*k1), (*q), t);
+            (*k1) *= (dt);
+            (*state_trans)((*q));
+            (*q) -= (*k0);
+            (*q) += (*k1);
+            (*inv_state_trans)((*q));
+            t += 0.5*dt;
+        }
+        
+        time_state_t& time(void) {return  t;}
+        var_state_t&  soln(void) {return *q;}
     };
 }
