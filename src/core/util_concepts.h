3e9ec6acedae56ca72c4681d2e1651c71b8e6f3d
Will <willvanzero@gmail.com>
2022-11-02 17:10:53 +0000

    add some concepts

---
+++ src/core/util_concepts.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <concepts>
+
+namespace spade::utils
+{
+    template <typename func_t, typename... args_t> concept const_callable = requires(const func_t& f, const args_t&... args)
+    {
+        f(args...);
+    };
+}
