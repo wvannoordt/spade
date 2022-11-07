f1cc51c2f824e14f892fdf590aea3fba06aa31cb
Will van Noordt <willvanzero@gmail.com>
2022-11-07 12:22:51 +0000

    ls

--- src/core/print.h
+++ src/core/print.h
@@ -14,38 +14,76 @@ namespace color
         blue    = 34,
         magenta = 35,
         cyan    = 36,
-        white   = 37//,
-        // default = 0
+        white   = 37,
+        none    = 0
     };
 }
 
-// static std::string color_format(const int& )
-
-static const std::string dlm = " ";
-template <typename T> static void print_recursive(std::ostream& stm, T t)
+static std::string color_format(const color::color& c)
 {
-    stm << t << std::endl;
+    return std::string("\033[0;" + std::to_string((int)c) + "m");
 }
 
-template <typename T, typename... Ts> static  void print_recursive(std::ostream& stm, T t, Ts... ts)
+static const std::string dlm = " ";
+
+namespace detail
 {
-    stm << t << dlm;
+    template <typename stream_t>
+    static void stream_content(stream_t& stm, const color::color& content)
+    {
+        stm << color_format(content);
+    }
+    
+    template <typename stream_t, typename content_t>
+    static void stream_content(stream_t& stm, const content_t& content)
+    {
+        stm << content;
+    }
+    
+    template <typename T> struct use_delim
+    {
+        static constexpr bool value = true;
+    };
+    template <> struct use_delim<color::color>
+    {
+        static constexpr bool value = false;
+    };
+    
+    template <typename T, typename... Ts> struct any_color
+    {
+        static constexpr bool value = std::same_as<T, color::color> || any_color<Ts...>::value;
+    };
+    template <typename T> struct any_color<T>
+    {
+        static constexpr bool value = std::same_as<T, color::color>;
+    };
+}
+
+template <typename T> static void print_recursive(std::ostream& stm, const T& t)
+{
+    detail::stream_content(stm, t);
+    stm << std::endl;
+}
+
+template <typename T, typename... Ts> static  void print_recursive(std::ostream& stm, const T& t, const Ts&... ts)
+{
+    detail::stream_content(stm, t);
+    if constexpr (detail::use_delim<T>::value)
+    {
+        detail::stream_content(stm, dlm);
+    }
     print_recursive(stm, ts...);
 }
 
-template <typename T, typename... Ts> static void print(const T& t, Ts... ts)
+template <typename T, typename... Ts> static void print(const T& t, const Ts&... ts)
 {
     print_recursive(std::cout, t, ts...);
+    if constexpr (detail::any_color<T, Ts...>::value) detail::stream_content(std::cout, color::none);
 }
 
 static void print(void)
 {
     std::cout << std::endl;
-}
-
-template <typename... Ts> static void PrintToStream(std::ostream& stm, Ts... ts)
-{
-    print_recursive(stm, ts...);
 }
 
 static inline std::string zfill(int num, int numZeros)
