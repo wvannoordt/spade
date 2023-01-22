1678aecb68a742f2b6af1eada696e38569fbb046
Will <willvanzero@gmail.com>
2023-01-22 12:50:07 +0000

    add exchange

---
+++ src/core/exchange.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "core/grid.h"
+#include "core/grid_array.h"
+
+namespace spade::parallel
+{
+    //todo: return a handle that can be awaited
+    template <typename array_t> void exchange_array(array_t& arr)
+    {
+        
+    }
+}
