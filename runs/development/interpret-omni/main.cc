86145abfc7f796c31f7917959fd8fbde29bc3639
wvn <willvanzero@gmail.com>
2023-04-03 18:24:22 +0100

    begine implementation of interpret

--- runs/development/interpret-omni/main.cc
+++ runs/development/interpret-omni/main.cc
@@ -35,8 +35,8 @@ int main(int argc, char** argv)
     //stencil 1:
     //
     //
-    //       c0    c1
-    //     |  +  |  +  |
+    //             c0    c1
+    //     |  +  |  +  |  +  |  +  |
     //
     //
     //
@@ -44,11 +44,11 @@ int main(int argc, char** argv)
         spade::grid::face_centered,
         spade::omni::elem_t<
             spade::omni::offset_t<-1,0,0>,
-            spade::omni::info_list_t<val_t,mtr_t>
+            spade::omni::info_list_t<val_t,mtr_t,nrm_t>
         >,
         spade::omni::elem_t<
             spade::omni::offset_t< 1,0,0>,
-            spade::omni::info_list_t<val_t,mtr_t>
+            spade::omni::info_list_t<val_t,mtr_t,nrm_t>
         >
     >;
 
@@ -84,6 +84,85 @@ int main(int argc, char** argv)
         >
     >;
 
+
+    using real_t = double;
+    spade::parallel::mpi_t group(&argc, &argv);
+    spade::ctrs::array<int, 3> num_blocks(2, 2, 2);
+    spade::ctrs::array<int, 3> cells_in_block(10, 10, 10);
+    spade::ctrs::array<int, 3> exchange_cells(2, 2, 2);
+    spade::bound_box_t<real_t, 3> bounds;
+    bounds.min(0) = 0.0;
+    bounds.max(0) = 2.0;
+    bounds.min(1) = 0.0;
+    bounds.max(1) = 2.0;
+    bounds.min(2) = 0.0;
+    bounds.max(2) = 2.0;
+
+    spade::coords::identity<real_t> coords;
+    spade::grid::cartesian_grid_t grid(num_blocks, cells_in_block, exchange_cells, bounds, coords, group);
+    spade::fluid_state::prim_t<real_t> fill = 0.0;
+    spade::grid::grid_array prim (grid, fill);
+    
+    using point_type = typename decltype(grid)::coord_point_type;
+    spade::algs::fill_array(prim, [](const point_type& x, const spade::grid::cell_idx_t& i)
+    {
+        spade::fluid_state::prim_t<real_t> output;
+        output.p() = real_t(i.i());
+        output.T() = real_t(i.j());
+        output.u() = real_t(i.k());
+        output.v() = real_t(i.lb());
+        output.w() = 1.0;
+        return output;
+    });
+
+    spade::io::output_vtk("temp", "io", prim);
+
+    //The two stencils disagree on the definition of c0, so when we union the stencils, things run amok
+    using ou_t = spade::omni::stencil_union<o1_t, o2_t>;
+
+    using array_t = decltype(prim);
+    using d1_t    = spade::omni::stencil_data_t<o1_t, array_t>;
+    using d2_t    = spade::omni::stencil_data_t<o2_t, array_t>;
+    using du_t    = spade::omni::stencil_data_t<ou_t, array_t>;
+
+    // print_type(ou_t());
+    print("==========================================");
+    print_type(d1_t());
+    print("==========================================");
+    print_type(d2_t());
+    print("==========================================");
+    print_type(du_t());
+    print("==========================================");
+
+    spade::grid::face_idx_t j;
+    j.i()   = 2;
+    j.j()   = 2;
+    j.k()   = 2;
+    j.lb()  = 0;
+    j.dir() = 0;
+    du_t data;
+    spade::omni::retrieve(prim, j, data);
+
+    // After retrieval, we need to reinterpret the data as relative to the original stencil definitions.
+
+    auto dat1 = spade::omni::interpret_stencil<o1_t>(data);
+    auto dat2 = spade::omni::interpret_stencil<o2_t>(data);
+
+    print("absolute stencil union:");
+    print(spade::omni::access<spade::omni::info::value>(data.cell(0_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(data.cell(0_c)));
+    print(spade::omni::access<spade::omni::info::value>(data.cell(1_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(data.cell(1_c)));
+    print(spade::omni::access<spade::omni::info::value>(data.cell(2_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(data.cell(2_c)));
+    print(spade::omni::access<spade::omni::info::value>(data.cell(3_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(data.cell(3_c)));
+
+    /*
+    print("relative to stencil 1:");
+    print(spade::omni::access<spade::omni::info::value>(dat1.cell(0_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(dat1.cell(0_c)));
+    print(spade::omni::access<spade::omni::info::value>(dat1.cell(1_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(dat1.cell(1_c)));
+
+    print("relative to stencil 2:");
+    print(spade::omni::access<spade::omni::info::value>(dat2.cell(2_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(dat2.cell(2_c)));
+    print(spade::omni::access<spade::omni::info::value>(dat2.cell(3_c)), "@ addr", &spade::omni::access<spade::omni::info::value>(dat2.cell(3_c)));
+    */
     
     return 0;
 }
--- src/omni/interpret.h
+++ src/omni/interpret.h
@@ -7,5 +7,19 @@
 
 namespace spade::omni
 {
-    
+    //Note that input_t could be an alias itself!
+    template <typename interpret_t, typename input_t>
+    struct stencil_alias_t
+    {
+        using stencil_type        = interpret_t; // used for any further (deeper) aliases
+        using native_stencil_type = typename input_t::stencil_type;
+        const input_t& native;
+        stencil_alias_t(const input_t& native_in) : native{native_in}{}
+    };
+
+    template <typename interpret_t, typename input_t>
+    auto interpret_stencil(const input_t& input)
+    {
+        return stencil_alias_t<interpret_t, input_t>(input);
+    }
 }
