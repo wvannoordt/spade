777a6e56fbfbbd2c0504de80d8ebdec86245c8e1
Will <willvanzero@gmail.com>
2024-02-07 20:41:22 -0500

    annoying source_location thing

--- src/core/config.h
+++ src/core/config.h
@@ -2,12 +2,20 @@
 
 #if(__cplusplus>=202002)
 #define C20_G11 1
-#ifndef USE_SOURCE_LOCATION
-#define USE_SOURCE_LOCATION 0
-#endif
 #else
 #define C20_G11 0
-#define USE_SOURCE_LOCATION 0
+#endif
+
+#include "cuda_incl.h"
+
+#if (_sp_cuda)
+#   if (CUDART_VERSION >= 12020)
+#       define USE_SOURCE_LOCATION 1
+#   else
+#       define USE_SOURCE_LOCATION 0
+#   endif
+#else
+#   define USE_SOURCE_LOCATION C20_G11
 #endif
 
 #include "core/c20.h"
